d73cf138b6bba96cd017a223621ed40ea8c6aa24
Tissiana Alves <altissiana@gmail.com>
2019-11-04 20:09:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,7 +8,7 @@ require 'directors_database'
 def directors_totals(nds)
   result = {}
     nds.each do |director|
-      director[director[:name]] = gross_for_director
+      director[director[:name]] = gross_for_director()
     end
 end
 
